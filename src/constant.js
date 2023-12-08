e5a03e03bf9013bce53ae711859dc2b8800b4260
Anshika31sharma <sharma.anshika7251@gmail.com>
2023-12-08 15:54:54 +0530

    add files

---
+++ src/constant.js
@@ -0,0 +1,58 @@
+import jhumka1 from "./assets/new.png";
+import jhumka2 from "./assets/n1.png";
+import jhumka3 from "./assets/n2.png";
+import jhumka4 from "./assets/n3.png";
+import jhumka5 from "./assets/n.png"
+import jhumka6 from "./assets/n4.png"
+export const SIZES = [10,15,20,25,30];
+export const QTY = [1, 2, 3, 4, 5];
+
+export const SHOE_LIST = [
+  {
+    id: 2,
+    src: jhumka1,
+    className: "bg-[#DDCEFD]",
+    title: "All About Jhumkas",
+    description:
+      "An online store that curates unique designs of jewellery from Different corners of India and delivered to your door step..",
+    price: 100,
+  },
+  
+  {
+    id: 1,
+    src: jhumka2,
+    className: "bg-[#FCC4EA]",
+    title: "Oxidised Silver",
+   
+  },
+  {
+    id: 3,
+    src: jhumka3,
+    className: "bg-[#DAFFA2]",
+    title: "Pastels",
+   
+  },
+  {
+    id: 2,
+    src: jhumka4,
+    className: "bg-[#DAFFA2]",
+    title: "Hoops",
+   
+  },
+  {
+    id: 4,
+    src: jhumka5,
+    className: "bg-[#FCC4EA]",
+    title: "Meenakari",
+    
+  },
+  {
+    id: 2,
+    src: jhumka6,
+    className
+    : "bg-[#DDCEFD]",
+    title: "Chaand Bali ",
+   
+  },
+];
+
